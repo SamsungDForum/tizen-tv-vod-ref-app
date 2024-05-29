123fb09b34ac4e1d70e6db2b08fda9737f82385c
Maciej Sawicki/Multichannel Video Services (PLT) /SRPOL/Staff Engineer/Samsung Electronics <m.sawicki@samsung.com>
2024-05-29 17:11:56 +0200

    Resume playback on player change (#350)
    fix

--- src/Components/usePlayerFactory/index.ts
+++ src/Components/usePlayerFactory/index.ts
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 import PlayerFactory from "./PlayerFactory";
 import type { PlayerConfig } from "./types/PlayerConfig";
-import { playAssetItemToString } from "./utils/playAssetCurrentTypes";
+import {
+  KeySystem,
+  Media,
+  playAssetItemToString,
+} from "./utils/playAssetCurrentTypes";
 import { IPlayer } from "./interfaces/IPlayer";
 import { useTypedSelector } from "../../reduxStore/useTypedSelector";
 
 const usePlayerFactory = (playerType: PlayerConfig) => {
   const [player, setPlayer] = useState<IPlayer | null | undefined>(null);
-  const media = useTypedSelector(state => state.playAsset.value.media);
-  const audio = useTypedSelector(state => state.playAsset.value.audio);
-  const subtitle = useTypedSelector(state => state.playAsset.value.subtitle);
-  const quality = useTypedSelector(state => state.playAsset.value.quality);
-  const keySystem = useTypedSelector(state => state.playAsset.value.keySystem);
+
+  const [playAsset, setPlayAsset] = useState<
+    [Media | null, KeySystem | undefined]
+  >([null, undefined]);
+  const media = useTypedSelector((state) => state.playAsset.value.media);
+  const audio = useTypedSelector((state) => state.playAsset.value.audio);
+  const subtitle = useTypedSelector((state) => state.playAsset.value.subtitle);
+  const quality = useTypedSelector((state) => state.playAsset.value.quality);
+  const keySystem = useTypedSelector(
+    (state) => state.playAsset.value.keySystem
+  );
 
   const destroyPlayer = () => PlayerFactory.destroy();
 
@@ -20,33 +30,45 @@ const usePlayerFactory = (playerType: PlayerConfig) => {
   }, []);
 
   useEffect(() => {
-    if(player != null && media != null) {
-      player.setAsset(media, keySystem);
+    if (player == null || playAsset[0] == null) return;
+    player.setAsset(playAsset[0], playAsset[1]);
+  }, [player]);
+
+  useEffect(() => {
+    if (media != null) {
+      if (player != null) {
+        player.setAsset(media, keySystem);
+      } else {
+        // Temp workaround.
+        // Player creation is async, react hook notifications will be received prior to player creation.
+        // Store asset till player is created.
+        setPlayAsset([media, keySystem]);
+      }
     }
   }, [media, keySystem]);
 
   useEffect(() => {
-    if(player != null && audio != null) {
+    if (player != null && audio != null) {
       player.changeCurrentAudio(audio);
       console.log(`audio has changed - ${playAssetItemToString(audio)}`);
     }
   }, [audio]);
-  
+
   useEffect(() => {
-    if(player != null && subtitle != null) {
+    if (player != null && subtitle != null) {
       player.changeCurrentSubtitles(subtitle);
       console.log(`subtitile has changed - ${playAssetItemToString(subtitle)}`);
     }
   }, [subtitle]);
-  
+
   useEffect(() => {
-    if(player != null && quality != null) { 
+    if (player != null && quality != null) {
       player.changeCurrentVideoQuality(quality);
       console.log(`quality has changed - ${playAssetItemToString(quality)}`);
     }
   }, [quality]);
-    
+
   return [player, destroyPlayer];
-}
+};
 
 export default usePlayerFactory;
